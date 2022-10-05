0a0ba50aabbdad38e39555509c132f2b98af9905
Imanrmd29 <iman.ramadhan@students.utdi.ac.id>
2022-10-05 11:17:55 +0700

    Initial commit

---
+++ app/src/main/java/com/iman/mybroadcastreceiver/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.iman.mybroadcastreceiver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.iman.mybroadcastreceiver.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        binding?.btnPermission?.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
